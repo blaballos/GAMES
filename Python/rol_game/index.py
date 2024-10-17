24c3cd93093b48b4a0afeb40687c76aa90987fbb
blaballos <laballosbrandon@gmail.com>
2024-10-17 13:36:17 -0300

    Add imports funcions to main

--- Python/rol_game/index.py
+++ Python/rol_game/index.py
@@ -1,10 +1,24 @@
-# IMPORTS
+from Functions.name_character import name_character
+from Functions.show_role import show_role
+from Functions.confirm_role import confirm_role
+from Functions.attack import attack
 
 def main():
     start = input('¡Welcome!\n1. Play\n2. Exit\nSelect an option: ')
     
     if start == '1':
-        print('Game') # start game
+        print('¡Perfect! crete your character')
+        name = name_character()
+        
+        role, confirmation = show_role()
+        while confirmation == '1':
+            role, confirmation = show_role()
+        character, enemy = confirm_role(name, role, confirmation)
+        
+        # testing
+        attack(character, enemy)
+        attack(enemy, character)
+        
     elif start == '2':
         print('¡See you later!')
         
