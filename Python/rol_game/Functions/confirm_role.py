22b4ea338d0f8b50325a221dca71e5234ca603b7
blaballos <laballosbrandon@gmail.com>
2024-10-17 13:29:41 -0300

    Add imports roles

--- Python/rol_game/Functions/confirm_role.py
+++ Python/rol_game/Functions/confirm_role.py
@@ -1,14 +1,17 @@
-# IMPORTS
+from Roles.warrior import Warrior
+from Roles.wizard import Wizard
+from Roles.golem import Golem
+from Roles.enemy import Enemy
 
 def confirm_role(name, role, confirmation):
     if confirmation == '2':
         if role == '1':
-            character = 'Warrior'
+            character = Warrior(name,290, 110, 1.2)
         elif role == '2':
-            character = 'Wizard'
+            character = Wizard(name,260, 130, 1.5)
         elif role == '3':
-            character = 'Golem'
+            character = Golem(name,400, 160, 2.3)
             
-        enemy = 'Enemy'
+        enemy = Enemy('Boss',1000, 200, 3)
         
     return character, enemy
