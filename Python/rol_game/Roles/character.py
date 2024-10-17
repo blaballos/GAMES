1ed02d122a369b04c638b4fb19779f41fd1c76a7
blaballos <laballosbrandon@gmail.com>
2024-10-17 12:38:28 -0300

    Create class character

---
+++ Python/rol_game/Roles/character.py
@@ -0,0 +1,8 @@
+# IMPORTS
+
+class Character():
+    def __init__(self, name, health, damage, attack_speed):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.attack_speed = attack_speed
