ae1f10a2d2a5d20f48762e6d3f4218febc05a4c1
blaballos <laballosbrandon@gmail.com>
2024-10-17 12:37:59 -0300

    Create user interface

---
+++ Python/rol_game/index.py
@@ -0,0 +1,11 @@
+# IMPORTS
+
+def main():
+    start = input('¡Welcome!\n1. Play\n2. Exit\nSelect an option: ')
+    
+    if start == '1':
+        print('Game') # start game
+    elif start == '2':
+        print('¡See you later!')
+        
+main()
