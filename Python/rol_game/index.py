c59db0342fdbfdd42ab64f9079bc9bb59ec9173a
blaballos <laballosbrandon@gmail.com>
2024-10-21 21:36:32 -0300

    Delete attack function

--- Python/rol_game/index.py
+++ Python/rol_game/index.py
@@ -1,7 +1,6 @@
 from Functions.name_character import name_character
 from Functions.show_role import show_role
 from Functions.confirm_role import confirm_role
-from Functions.attack import attack
 
 def main():
     start = input('¡Welcome!\n1. Play\n2. Exit\nSelect an option: ')
@@ -14,10 +13,6 @@ def main():
         while confirmation == '1':
             role, confirmation = show_role()
         character, enemy = confirm_role(name, role, confirmation)
-        
-        # testing
-        attack(character, enemy)
-        attack(enemy, character)
         
     elif start == '2':
         print('¡See you later!')
