02b34e252fd397f20143252ae6a34368ccc51758
blaballos <laballosbrandon@gmail.com>
2024-10-21 21:36:07 -0300

    Add attack and defend functions in character class

--- Python/rol_game/Roles/character.py
+++ Python/rol_game/Roles/character.py
@@ -1,9 +1,25 @@
-from Actions.attack import Attack
-from Actions.defend import Defend
+import random
 
-class Character(Attack, Defend):
+class Character():
     def __init__(self, name, health, damage, attack_speed):
         self.name = name
         self.health = health
         self.damage = damage
         self.attack_speed = attack_speed
+        
+    def attack(self,health_enemy):
+        random_number = random.randint(0,7)
+        if random_number == 0:
+            print(f'{self.name} his attack failed')
+        else:
+            print(f'{self.name} performed a hit of {self.damage} damage')
+            health_enemy -= self.damage
+            
+        
+    def defend(self, damage_enemy):
+        random_number = random.randint(0,7)
+        if random_number == 0:
+            print(f'{self.name} could not avoid the hit')
+            self.health -= damage_enemy
+        else:
+            print(f'{self.name} successfully defended himself')
